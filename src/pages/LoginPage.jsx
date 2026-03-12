aaaf4c262b3a204702fe647252237e23ddff8c07
divyamhacker123 <divyam.hata1@gmail.com>
2026-03-12 12:38:53 +0530

    Added new chat UI component

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import assets from '../assets/assets'
 
 const LoginPage = () => {
+
+  const [currState, setcurrState] = useState("Sign up")
+  const [fullName, setfullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("")
+  const [bio, setBio] = useState("");
+  const [isDataSubmitted, setIsDataSubmitted] = useState(false);
+
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
-      {/* {---------left-------------} */}
-        <img src={assets.logo_big} alt="" className='w-[min(30vw, 250px)]'/>
+      
+      {/* --------- left --------- */}
+      <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]'/>
 
-        {/* {-------------- right ------------} */}
-        <form action=""></form>
+      {/* --------- right -------- */}
+      <form className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+        
+        <h2 className='font-medium text-2xl flex justify-between items-center'>
+          {currState}
+          <img src={assets.arrow_icon} alt="" className='w-5 cursor-pointer' />
+        </h2>
+        { 
+        currState == 'Sign up' &&( 
+        <input type="text" className='p-2 border border-gray-500 rounded-md focus:outline-none' placeholder=' Full Name ' required/>
+        )}
+      </form>
+
     </div>
   )
 }
