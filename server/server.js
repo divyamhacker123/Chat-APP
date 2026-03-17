88b8b6c1b4aea0b267f5e05e459da45121088835
divyamhacker123 <divyam.hata1@gmail.com>
2026-03-17 23:42:25 +0530

    Added MongoDB

--- server/server.js
+++ server/server.js
@@ -5,11 +5,27 @@ import express from "express"
 import "dotenv/config"
 import cors from "cors"
 import http from "http"
+// import { prototype } from "events";
 
 // create express app and HTTP Server
 const app = express();
 
 // because socket.io use http server
-const server = http.createServer();
+const server = http.createServer(app);
+
+// Middleware Setup
+// request can be parse into json format 
+// so images cannot exceed 4 mb 
+app.use(express.json({limit: "4mb"}));
+app.use(cors());
+app.use("/api/status", (req, res)=> {
+    res.send("Server is live")
+})
+
+// if the port number is available in the environment variable 
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, ()=>{
+     console.log("Server is running on Port : ", PORT);
+})
 
 
